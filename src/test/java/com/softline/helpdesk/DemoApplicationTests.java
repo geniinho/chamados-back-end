e9f7744e23679273166b3a77955904dd5f04e9e8
geniinho <geniinho@hotmail.com>
2021-08-25 18:19:13 -0300

    projeto iniciado

---
+++ src/test/java/com/softline/helpdesk/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softline.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
