5e95c9be6bba4b3175aa3fbe63963892d088a329
geniinho <geniinho@hotmail.com>
2021-08-28 09:53:13 -0300

    removendo serialize no endpoint /tecnico

---
+++ src/main/java/com/softline/helpdesk/services/TecnicoService.java
@@ -0,0 +1,22 @@
+package com.softline.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.softline.helpdesk.domain.Tecnico;
+import com.softline.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class TecnicoService {
+
+	
+	@Autowired
+	private TecnicoRepository repository;
+	
+	public Tecnico findById(Integer id) {
+		Optional<Tecnico> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+}
