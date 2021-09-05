5d865473aae67f913a2bcebb44ecb036e8352e47
geniinho <geniinho@hotmail.com>
2021-09-05 11:45:39 -0300

    endpoint para delete do tecnico

--- src/main/java/com/softline/helpdesk/services/TecnicoService.java
+++ src/main/java/com/softline/helpdesk/services/TecnicoService.java
@@ -50,6 +50,15 @@ public class TecnicoService {
 		return repository.save(oldObj);
 		
 	}
+	
+	public void delete(Integer id) {
+		Tecnico obj = findById(id);
+		if(obj.getChamados().size()>0) {
+			throw new DataIntegrityViolationException("O técnico possui ordens de serviço e não pode ser deletado!");
+		}
+		repository.deleteById(id);
+		
+	}
 
 	private void validaPorCpfEEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -62,6 +71,8 @@ public class TecnicoService {
 			throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
 		}
 	}
+
+	
 
 
 }
