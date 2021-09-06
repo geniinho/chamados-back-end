4f25b0f8ea5635c4551e36aa15b484e24a4c4da2
geniinho <geniinho@hotmail.com>
2021-09-06 11:15:08 -0300

    endpoint para criar chamado

--- src/main/java/com/softline/helpdesk/services/ChamadoService.java
+++ src/main/java/com/softline/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.softline.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.softline.helpdesk.domain.Chamado;
+import com.softline.helpdesk.domain.Cliente;
+import com.softline.helpdesk.domain.Tecnico;
+import com.softline.helpdesk.domain.dtos.ChamadoDTO;
+import com.softline.helpdesk.domain.enums.Prioridade;
+import com.softline.helpdesk.domain.enums.Status;
 import com.softline.helpdesk.repositories.ChamadoRepository;
 import com.softline.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,6 +22,11 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
+	
 	
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
@@ -27,4 +39,25 @@ public class ChamadoService {
 		return repository.findAll();
 	}
 	
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+	
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+		
+		Chamado chamado = new Chamado();
+		if(obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+		
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+	}
 }
