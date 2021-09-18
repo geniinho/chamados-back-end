aa6db737c901748646410cb9db182e00eadc8457
geniinho <geniinho@hotmail.com>
2021-09-18 08:55:28 -0300

    ajuste para atualizar senha

--- src/main/java/com/softline/helpdesk/services/ClienteService.java
+++ src/main/java/com/softline/helpdesk/services/ClienteService.java
@@ -49,6 +49,11 @@ public class ClienteService {
 	public Cliente update(Integer id, @Valid ClienteDTO objDTO) {
 		objDTO.setId(id);
 		Cliente oldObj = findById(id);
+		
+		if(!objDTO.getSenha().equals(oldObj.getSenha())) { //verifica se a senha foi alterada, caso seja encoda novamente
+			objDTO.setSenha(encoder.encode(objDTO.getSenha()));
+		}
+		
 		validaPorCpfEEmail(objDTO);
 		oldObj = new Cliente(objDTO);
 		return repository.save(oldObj);
